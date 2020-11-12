6508ee24c30d9e3f4dba2a82984dd70f69617b39
www42 <thomas.jaekel@gfn.de>
2020-11-12 18:33:28 +0100

    updated

--- CreateVM.ps1
+++ CreateVM.ps1
@@ -21,8 +21,26 @@ $pipName = "$vmName-PIP"
 
 
 # Create
+New-AzResourceGroup -Name $rgName -Location $location
+Get-AzResourceGroup | Format-Table ResourceGroupName,Location,ProvisioningState
 
-# To be done
+New-AzVm `
+    -Name $vmName `
+    -ResourceGroupName $rgName `
+    -Location $location `
+    -VirtualNetworkName $vnetName `
+    -SubnetName $subnetName `
+    -Credential $credential `
+    -SecurityGroupName $nsgName `
+    -PublicIpAddressName $pipName `
+    -OpenPorts 80,3389
+
+
+Get-AzVM
+Get-AzVM -Status | Format-Table Name,ResourceGroupName,Location,PowerState
+
+Get-AzResource -ResourceGroupName $rgName | Format-Table Name,ResourceType
+
 
 # Clean up
 Remove-AzResourceGroup -Name $rgName -Force -AsJob
