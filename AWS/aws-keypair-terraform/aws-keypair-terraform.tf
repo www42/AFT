c5202fd45940357fc3fd52e3e98dcb67adb8b678
www42 <thomas.jaekel@gfn.de>
2020-12-31 14:52:44 +0100

    key pair tf ready

--- AWS/aws-keypair-terraform/aws-keypair-terraform.tf
+++ AWS/aws-keypair-terraform/aws-keypair-terraform.tf
@@ -11,14 +11,23 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# ssh-keygen -f foo3 -t rsa -b 2048 -m pem -C 'foo3@example.com' -P ''
-# chmod 400 foo3
-# openssl rsa -in foo3 -pubout -outform DER 2>/dev/null | openssl md5 -c
-# cat foo3.pub
+/*   Generate key pair
+ssh-keygen -f foo3 -t rsa -b 2048 -m pem -C 'foo3@example.com' -P ''
+chmod 400 foo3
+openssl rsa -in foo3 -pubout -outform DER 2>/dev/null | openssl md5 -c
+cat foo3.pub
+*/
 
 resource "aws_key_pair" "foo3key" {
   key_name   = "foo3key"
   public_key = ""
 }
 
-# aws ec2 describe-key-pairs --filters Name=key-name,Values=foo3key
+/*   Proof key was uploaded by tf
+aws ec2 describe-key-pairs --filters Name=key-name,Values=foo3key
+*/
+
+
+/*   Clean up
+rm -rf .terraform*  terraform.tfstate*   foo3*
+*/
