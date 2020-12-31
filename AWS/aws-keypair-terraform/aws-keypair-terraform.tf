dfeda58c29a7024dcccbace3875c3ab82dfa98f2
www42 <thomas.jaekel@gfn.de>
2020-12-31 23:00:16 +0100

    reordered

--- AWS/aws-keypair-terraform/aws-keypair-terraform.tf
+++ AWS/aws-keypair-terraform/aws-keypair-terraform.tf
@@ -1,3 +1,10 @@
+/*   Generate key pair
+ssh-keygen -f foo3 -t rsa -b 2048 -m pem -C 'foo3@example.com' -P ''
+chmod 400 foo3
+openssl rsa -in foo3 -pubout -outform DER 2>/dev/null | openssl md5 -c
+cat foo3.pub
+*/
+
 terraform {
   required_providers {
     aws = {
@@ -11,19 +18,12 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-/*   Generate key pair
-ssh-keygen -f foo3 -t rsa -b 2048 -m pem -C 'foo3@example.com' -P ''
-chmod 400 foo3
-openssl rsa -in foo3 -pubout -outform DER 2>/dev/null | openssl md5 -c
-cat foo3.pub
-*/
-
 resource "aws_key_pair" "foo3key" {
   key_name   = "foo3"
   public_key = ""
 }
 
-/*   Proof key was uploaded by tf
+/*   The proof of the pudding
 aws ec2 describe-key-pairs --filters Name=key-name,Values=foo3key
 */
 
