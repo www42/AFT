782fc14c27f59908981951380ba7817ecf6fc86a
www42 <thomas.jaekel@gfn.de>
2021-01-02 12:04:51 +0100

    cleanup for gh-pages

--- AWS/securitygroup.sh
+++
@@ -1,3 +0,0 @@
-aws ec2 describe-security-groups --query "SecurityGroups[*].{Description:Description,VpcId:VpcId}" --output table
-
-aws ec2 describe-vpcs --query "Vpcs[*].{IsDefault:IsDefault,VpcId:VpcId}"
--- AWS/vpc.sh
+++
@@ -1,9 +0,0 @@
-
-# VPCs
-aws ec2 describe-vpcs --query "Vpcs[*].{VpcId:VpcId,CidrBlock:CidrBlock,State:State}" --output table
-aws ec2 delete-vpc --vpc-id vpc-099a157de9fc1d4a4
-
-aws ec2 create-vpc \
-    --cidr-block 10.0.0.0/16 --amazon-provided-ipv6-cidr-block
-
-aws ec2 describe-vpcs --query "Vpcs[*].{VpcId:VpcId,CidrBlock:CidrBlock,State:State,Ipv6CidrBlock:Ipv6CidrBlockAssociationSet[0].Ipv6CidrBlock}" --output table
