b5234f3fa7b41f843e6264035218450d4770e8a4
www42 <thomas.jaekel@gfn.de>
2021-01-03 12:40:40 +0100

    git folder added

---
+++ Git/gh-pages.sh
@@ -0,0 +1,19 @@
+
+git branch --list
+git branch --list -a
+
+git branch gh-pages
+git checkout gh-pages
+
+# rm *
+
+echo "<h1>Happy New Year</h1>" > index.html
+
+git add index.html
+git commit -a -m "happy new year"
+
+git push --set-upstream origin gh-pages
+git push
+
+# git checkout main
+# git branch -d gh-pages
